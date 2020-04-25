6809395c0b31bee0f7bc6190e09a63427c29d1c2
Cody Lundquist <cody@getfeedback.com>
2020-04-25 10:23:21 -0700

    Update README.md. Better dev environment.
    Remove the Public API Keys and config from the code and pull it from the dynamic reserved URLs from firebase.  In doing that, we have to also run the firebase dev server and proxy unknown requests (i.e. /__/firebase/*) to it so they can be served.  In production, Firebase itself will handle this.

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,21 +1,18 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/analytics";
-import "firebase/firestore";
+import firebase from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA7Wgy8GRvyIxBnHS59WLaq3KCEIqUALk4",
-  authDomain: "codysnames-game.firebaseapp.com",
-  databaseURL: "https://codysnames-game.firebaseio.com",
-  projectId: "codysnames-game",
-  storageBucket: "codysnames-game.appspot.com",
-  messagingSenderId: "596970333106",
-  appId: "1:596970333106:web:42ae9bc40e8750d32afaab",
-  measurementId: "G-VDK8GFNT0W",
-};
+// We are loading Firebase from the dynamic reserved URLS
+// but we still want the typed libraries.  Webpack is smart
+// enough not to actually bundle firebase.
+declare global {
+  interface Window {
+    firebase: {
+      auth: () => firebase.auth.Auth;
+      firestore: () => firebase.firestore.Firestore;
+      analytics: () => firebase.analytics.Analytics;
+    };
+  }
+}
 
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export const analytics = firebase.analytics();
+export const auth = window.firebase.auth();
+export const db = window.firebase.firestore();
+export const analytics = window.firebase.analytics();
